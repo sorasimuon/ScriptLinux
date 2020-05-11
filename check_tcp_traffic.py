d82a54ac267fd28057761df0d1ac9463bdc3d18a
sorasimuon <sorasi.muon@gmail.com>
2020-05-11 21:53:38 +0800

    Add script to check tcp messaging

---
+++ check_tcp_traffic.py
@@ -0,0 +1,24 @@
+from subprocess import Popen, PIPE
+from datetime import datetime
+import dpkt
+
+
+def run_write_tcp_traffic(options):
+    '''
+    This function is used to check the TCP/UDP Traffic occuring on the current machine
+    :param options:
+    :return: create a pcap file ready for analysis
+    '''
+
+    dest = options.destination
+    source = options.sources
+    nb_pk = options.nb_pk
+    port = options.port
+    interface = options.interface
+
+
+    p = Popen("tcpdump -i {} -t -c {} -w {} \"port {}\"".format(interface, nb_pk, "$HOME/Logs/tcp_traffic_-_{}.pcap".format(datetime.now().strftime("%Y%m%d_-_%H%M%S")), port), shelll=True)
+
+if __name__ == "__main__":
+    
+    run_write_tcp_traffic()
---
+++ custom_tcpdump.py
@@ -0,0 +1,29 @@
+from optparse import OptionParser
+from check_tcp_traffic import *
+
+parser = OptionParser()
+
+parser.add_option("-d", "--destination",
+                  dest="destination",
+                  help="define the packet IP destination")
+
+parser.add_option("-s", "--source",
+                  dest="source",
+                  help="Define the packet IP source")
+parser.add_option("-c", "--count",
+                  dest="nb_pk",
+                  type=int,
+                  default=20,
+                  help="Define the number of packets to collect")
+parser.add_option("-p", "--port",
+                  dest="port",
+                  type=int,
+                  help="Define the port to listen")
+parser.add_option("-i", "--interface",
+                  dest="interface",
+                  help="Define the network interface to analyze")
+
+(options, args) = parser.parse_args()
+
+run_write_tcp_traffic(options)
+
