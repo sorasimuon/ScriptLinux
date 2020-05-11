f628d131d5d66c23a6db401950ae5fdb3071108c
sorasimuon <sorasi.muon@gmail.com>
2020-05-11 22:12:19 +0800

    typo on sources -> source

--- check_tcp_traffic.py
+++ check_tcp_traffic.py
@@ -11,14 +11,10 @@ def run_write_tcp_traffic(options):
     '''
 
     dest = options.destination
-    source = options.sources
+    source = options.source
     nb_pk = options.nb_pk
     port = options.port
     interface = options.interface
 
 
     p = Popen("tcpdump -i {} -t -c {} -w {} \"port {}\"".format(interface, nb_pk, "$HOME/Logs/tcp_traffic_-_{}.pcap".format(datetime.now().strftime("%Y%m%d_-_%H%M%S")), port), shelll=True)
-
-if __name__ == "__main__":
-
-    run_write_tcp_traffic()
