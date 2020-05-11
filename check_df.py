07d1a3eb05f043707ec18e74a6582b1fc1354ce7
sorasimuon <sorasi.muon@gmail.com>
2020-05-11 16:41:48 +0800

    change on check_df

--- check_df.py
+++ check_df.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python
 
-from subprocess import Popen
+from subprocess import Popen, PIPE
 
 def run_df():
-    df = Popen()
+    df = Popen(["df", "-h"], stdout=PIPE)
+    list_fs = {}
+    for bytes in df.stdout:
+        line = bytes.decode()
+
+        # get the header
+        if line.startswith("filesystem"):
+            header = line
+
+        if line.startswith("/dev/sd"):
+            fs_line = line.split()
+            list_fs[fs_line[0]] = int(fs_line[4][:-1])
+
+    # Print the result to stdout
+    print(header)
+    for k,v in list_fs.items():
+        print("{} is {}% full.".format(k,v))
+        
+
+
+
+
+
